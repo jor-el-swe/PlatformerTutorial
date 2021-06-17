68b182a61d2a5915c2763904b6f13d480c29e266
Joel Ring <joel.ring@gmail.com>
2021-06-17 22:11:33 +0200

    adds character png:s and basic player controller

---
+++ Attila.gd
@@ -0,0 +1,15 @@
+extends KinematicBody2D
+
+var velocity = Vector2(0,0)
+
+
+func _physics_process(delta):
+	if Input.is_action_pressed("right"):
+		velocity.x = 100
+		
+	if Input.is_action_pressed("left"):
+		velocity.x = -100
+
+	velocity.x = lerp(velocity.x,0,0.1)
+	move_and_slide(velocity)
+
