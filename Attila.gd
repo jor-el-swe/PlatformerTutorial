1d4cca05f8ec7db861a5d9793680dd264ee63f91
Joel Ring <joel.ring@gmail.com>
2021-06-19 10:04:39 +0200

    adds player animations

--- Attila.gd
+++ Attila.gd
@@ -11,11 +11,20 @@ func _physics_process(delta):
 	var totalGravity = GRAVITY;
 	if Input.is_action_pressed("right"):
 		velocity.x = SPEED
+		$Sprite.play("walk")
+		$Sprite.flip_h = false
 		
-	if Input.is_action_pressed("left"):
+	elif Input.is_action_pressed("left"):
 		velocity.x = -SPEED
+		$Sprite.flip_h = true
+		$Sprite.play("walk")
+	
+	else: 
+		$Sprite.play("idle")
 		
 
+	if !is_on_floor():
+		$Sprite.play("air")
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_SPEED
