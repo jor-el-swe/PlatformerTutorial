96485879bf6ecdcb678566e17826af4204f8f4a3
Joel Ring <joel.ring@gmail.com>
2021-06-27 21:28:40 +0200

    adds enemy hurt zone

--- enemy.gd
+++ enemy.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 var velocity = Vector2(0,0)
 export var direction = -1
 export var detectsCliffs = true
-const ENEMY_SPEED = 50
+var speed = 50
 const GRAVITY = 20
 
 func _ready():
@@ -21,9 +21,22 @@ func _physics_process(delta):
 		$AnimatedSprite.flip_h = !$AnimatedSprite.flip_h 
 		$floorChecker.position.x = $CollisionShape2D.shape.get_extents().x * direction
 	
-	
-	
 	velocity.y += GRAVITY
-	velocity.x = ENEMY_SPEED * direction
+	velocity.x = speed * direction
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
+
+
+func _on_topChecker_body_entered(body):
+	$AnimatedSprite.play("squashed")
+	speed = 0
+	set_collision_mask_bit(0, false)
+	set_collision_layer_bit(4, false)
+	$sidesChecker.set_collision_mask_bit(0, false)
+	$sidesChecker.set_collision_layer_bit(4, false)
+	
+
+
+func _on_sidesChecker_body_entered(body):
+	print("hit player")
+	get_tree().change_scene("res://Level1.tscn")
