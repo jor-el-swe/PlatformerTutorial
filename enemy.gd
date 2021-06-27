20dd43984c2d08b1d9eaff4cec0aa13c65f94959
Joel Ring <joel.ring@gmail.com>
2021-06-27 12:59:33 +0200

    enemy turning working

--- enemy.gd
+++ enemy.gd
@@ -8,12 +8,21 @@ const GRAVITY = 20
 func _ready():
 	if direction == 1:
 		$AnimatedSprite.flip_h = true
+	
+	$floorChecker.position.x = $CollisionShape2D.shape.get_extents().x * direction
 		
 	
 
 func _physics_process(delta):
 
+	if is_on_wall() or not$floorChecker.is_colliding() and is_on_floor():
+		direction *=-1
+		$AnimatedSprite.flip_h = !$AnimatedSprite.flip_h 
+		$floorChecker.position.x = $CollisionShape2D.shape.get_extents().x * direction
+	
+	
+	
 	velocity.y += GRAVITY
 	velocity.x = ENEMY_SPEED * direction
 	
-	move_and_slide(velocity)
+	velocity = move_and_slide(velocity, Vector2.UP)
