5483de9d2b9701034b7a91a849ba3b299d8b3142
Joel Ring <joel.ring@gmail.com>
2021-06-27 13:05:59 +0200

    fixes enemies

--- enemy.gd
+++ enemy.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 var velocity = Vector2(0,0)
 export var direction = -1
+export var detectsCliffs = true
 const ENEMY_SPEED = 50
 const GRAVITY = 20
 
@@ -10,12 +11,12 @@ func _ready():
 		$AnimatedSprite.flip_h = true
 	
 	$floorChecker.position.x = $CollisionShape2D.shape.get_extents().x * direction
-		
+	$floorChecker.enabled = detectsCliffs	
 	
 
 func _physics_process(delta):
 
-	if is_on_wall() or not$floorChecker.is_colliding() and is_on_floor():
+	if is_on_wall() or not$floorChecker.is_colliding() and detectsCliffs and is_on_floor():
 		direction *=-1
 		$AnimatedSprite.flip_h = !$AnimatedSprite.flip_h 
 		$floorChecker.position.x = $CollisionShape2D.shape.get_extents().x * direction
