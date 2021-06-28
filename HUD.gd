33e9c9059d9a3ee5734e91c9a1e5b105830bbe92
Joel Ring <joel.ring@gmail.com>
2021-06-28 15:33:11 +0200

    fixes game over screen

--- HUD.gd
+++ HUD.gd
@@ -6,11 +6,9 @@ export var NO_COINS_TO_WIN = 6
 func _ready():
 	$Coins.text = String(coins)
 	
-func _physics_process(delta):
-	if coins == NO_COINS_TO_WIN:
-		get_tree().change_scene("res://WinScreen.tscn")
-
 
 func _on_coin_collected():
 	coins+=1
 	_ready()
+	if coins == NO_COINS_TO_WIN:
+		get_tree().change_scene("res://WinScreen.tscn")
